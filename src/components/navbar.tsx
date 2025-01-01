dcf2640917c597e028620159382d8b5e58ff7b6c
daffasatrianegara <daffasatria036@gmail.com>
2025-01-02 00:22:23 +0700

    feat: add navbar (done)

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,70 @@
 import { ModeToggle } from "./mode-toggle";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { SiInstagram, SiLinkedin, SiSpotify, SiGithub } from "react-icons/si";
 
-const NavbarComponent = () => {
+const NavbarComponent: React.FC = () => {
+  const handleRedirectToSocmed = (link: string) => {
+    window.open(link, "_blank", "noopener,noreferrer");
+  };
+
   return (
     <div className="w-full sticky top-0 px-5 flex items-center py-3 border-gray-200 dark:border-gray-800 border-b-[1px] backdrop-blur bg-white/30 dark:bg-gray-900/30 z-50">
       <p className="font-semibold text-lg mr-auto max-md:text-base">
-        Muhammad Daffa Satria Negara
+        Daffa Satria
       </p>
-      <div className="ms-auto">
+      <div className="ms-auto items-center flex gap-3">
+        <DropdownMenu>
+          <DropdownMenuTrigger className="text-sm text-white">
+            Hit Me Up!
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Let's Connect!</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              onClick={() =>
+                handleRedirectToSocmed(
+                  "https://www.linkedin.com/in/muhammad-daffa-satria-negara",
+                )
+              }
+            >
+              <SiLinkedin />
+              Linkedin
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() =>
+                handleRedirectToSocmed("https://github.com/daffasatrianegara")
+              }
+            >
+              <SiGithub/>
+              Github
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() =>
+                handleRedirectToSocmed("https://www.instagram.com/dapaasat/")
+              }
+            >
+              <SiInstagram/>
+              Instagram
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() =>
+                handleRedirectToSocmed(
+                  "https://open.spotify.com/user/31ilmpqdgjqh7g45uwnoofkyb4za?si=etzrVEZVSlKlmzSeKHv9kQ",
+                )
+              }
+            >
+              <SiSpotify/>
+              Spotify
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <ModeToggle />
       </div>
     </div>
