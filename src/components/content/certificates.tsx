97a0ee635f8c4404f6e0fcd6f3a0849ee7ed9220
daffasatrianegara <daffasatria036@gmail.com>
2025-01-08 22:29:43 +0700

    feat: make certificates data dynamic

--- src/components/content/certificates.tsx
+++ src/components/content/certificates.tsx
@@ -1,46 +1,31 @@
 import { Card } from "../ui/card";
+import CertificatesData from "@/contents/certificates/index.json";
 
 const CertificatesContent = () => {
+  const contentData = CertificatesData.certificates.slice(0, 3);
+
   return (
     <Card className="p-5">
       <p className="text-base md:text-lg font-semibold">Certificates</p>
       <div className="w-full flex flex-col gap-2 mt-2">
-        <div>
-          <a
-            className="text-sm hover:underline font-medium"
-            href="https://www.dicoding.com/certificates/2VX347GNNZYQ"
-            target="_blank"
-          >
-            <span className="text-emerald">Certificate of {"Competency"}</span>{" "}
-            - Belajar Penerapan Machine Learning dengan Google Cloud
-          </a>
-          <p className="text-xs my-1">21 Desember 2024 - 21 Desember 2027</p>
-          <p className="text-xs my-1">Dicoding Indonesia</p>
-        </div>
-        <div>
-          <a
-            className="text-sm hover:underline font-medium"
-            href="https://www.dicoding.com/certificates/2VX347GNNZYQ"
-            target="_blank"
-          >
-            <span className="text-emerald">Certificate of {"Competency"}</span>{" "}
-            - Belajar Penerapan Machine Learning dengan Google Cloud
-          </a>
-          <p className="text-xs my-1">21 Desember 2024 - 21 Desember 2027</p>
-          <p className="text-xs my-1">Dicoding Indonesia</p>
-        </div>
-        <div>
-          <a
-            className="text-sm hover:underline font-medium"
-            href="https://www.dicoding.com/certificates/2VX347GNNZYQ"
-            target="_blank"
-          >
-            <span className="text-emerald">Certificate of {"Competency"}</span>{" "}
-            - Belajar Penerapan Machine Learning dengan Google Cloud
-          </a>
-          <p className="text-xs my-1">21 Desember 2024 - 21 Desember 2027</p>
-          <p className="text-xs my-1">Dicoding Indonesia</p>
-        </div>
+        {contentData.map((data, index) => (
+          <div key={index}>
+            <a
+              className="text-sm hover:underline font-medium"
+              href={data.url}
+              target="_blank"
+            >
+              <span className="text-emerald">Certificate of {data.status}</span>{" "}
+              - {data.title}
+            </a>
+            <p className="text-xs my-1">
+              {data.end_date === ""
+                ? `${data.published_date}`
+                : `${data.published_date} – ${data.end_date}`}
+            </p>
+            <p className="text-xs my-1">{data.company_name}</p>
+          </div>
+        ))}
         <div className="w-full border-b my-1"></div>
         <div className="flex justify-center">
           <a
