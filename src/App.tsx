a066e221df3cb7b9aff7da079ffc06bd80a6585f
daffasatrianegara <daffasatria036@gmail.com>
2025-01-10 22:30:59 +0700

    feat: add experiences page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,87 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
 import { HelmetProvider } from "react-helmet-async";
+import { AnimatePresence, motion } from "framer-motion";
+
 import LandingPage from "./pages/landing.page";
 import CertificatesPage from "./pages/certificates";
 import ProjectsPage from "./pages/projects";
 import DetailProjectPage from "./pages/detail.project";
 import ExperiencesPage from "./pages/experiences";
 
+const PageWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <motion.div
+    initial={{ opacity: 0, y: 50 }}
+    animate={{ opacity: 10, y: 0 }}
+    exit={{ opacity: 0, y: -20 }}
+    transition={{ duration: 1 }}
+  >
+    {children}
+  </motion.div>
+);
+
+const AnimatedRoutes: React.FC = () => {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <PageWrapper>
+              <LandingPage />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/certificates"
+          element={
+            <PageWrapper>
+              <CertificatesPage />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/projects"
+          element={
+            <PageWrapper>
+              <ProjectsPage />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/projects/:id"
+          element={
+            <PageWrapper>
+              <DetailProjectPage />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/experiences"
+          element={
+            <PageWrapper>
+              <ExperiencesPage />
+            </PageWrapper>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
+  );
+};
+
 function App() {
   return (
     <HelmetProvider>
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
         <Router>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/certificates" element={<CertificatesPage />} />
-            <Route path="/projects" element={<ProjectsPage />} />
-            <Route path="/projects/:id" element={<DetailProjectPage />} />
-            <Route path="/experiences" element={<ExperiencesPage />} />
-          </Routes>
+          <AnimatedRoutes />
         </Router>
       </ThemeProvider>
     </HelmetProvider>
--- src/pages/experiences.tsx
+++ src/pages/experiences.tsx
@@ -1,5 +1,77 @@
+import MainLayout from "@/components/layouts/main.layout";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import ExperiencesData from "@/contents/experiences/index.json";
+
 const ExperiencesPage = () => {
-  return <h1>ini halaman experiences</h1>;
+  const contentData = ExperiencesData.experiences;
+
+  return (
+    <MainLayout title="Experiences | Muhammad Daffa Satria Negara">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Experiences</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <div className="my-5">
+        <p className="text-xl md:text-2xl font-semibold">
+          Profesional Experiences
+        </p>
+        <div className="w-full border-b mt-2 border-gray-200 dark:border-gray-800"></div>
+      </div>
+      <div className="ms-2">
+        <ol className="relative border-s border-emerald dark:border-gray-700">
+          {contentData.map((data, index) => (
+            <li className="mb-10 ms-4" key={index}>
+              <div className="absolute w-3 h-3 bg-emerald rounded-full mt-2 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+              <time className="mb-1 text-sm md:text-base font-normal leading-none text-gray-400 dark:text-gray-500">
+                {data.start_date} - {data.end_Date}
+              </time>
+              <p className="text-lg md:text-xl font-semibold text-gray-900 dark:text-white">
+                {data.position}{" "}
+                <span className="text-sm md:text-base font-normal text-gray-400 dark:text-gray-300">
+                  - {data.status}
+                </span>
+              </p>
+              <p className="text-sm md:text-base text-gray-400 dark:text-gray-300 my-0.5">
+                {data.company_name}
+              </p>
+              <p className="text-sm md:text-base font-normal text-gray-500 dark:text-gray-400">
+                {data.text_intro} <br />
+                {data.description_role.length > 0 ? (
+                  <ul className="mt-1 ms-2">
+                    {data.description_role.map((data, index) => (
+                      <li
+                        key={index}
+                        className="grid grid-cols-[auto,1fr] gap-x-2 items-start my-1"
+                      >
+                        <span className="list-disc">•</span>
+                        <span>{data}</span>
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <></>
+                )}
+              </p>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </MainLayout>
+  );
 };
 
 export default ExperiencesPage;
