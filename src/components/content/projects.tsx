f048646d1bdfe4d345559ad1bf9e10d09a3712bc
daffasatrianegara <daffasatria036@gmail.com>
2025-01-13 18:34:13 +0700

    update

--- src/components/content/projects.tsx
+++ src/components/content/projects.tsx
@@ -11,7 +11,7 @@ const ProjectsContent = ({
       <div className="mt-2 md:mt-4 w-full">
         <div className="p-1">
           <div className="grid grid-cols-6 md:grid-cols-5 gap-3 md:gap-5">
-            <div className="col-span-2 max-[457px]:col-span-6 md:col-span-1">
+            {/* <div className="col-span-2 max-[457px]:col-span-6 md:col-span-1">
               <img
                 className="w-full object-cover rounded-md"
                 src="/img/test.png"
@@ -23,7 +23,7 @@ const ProjectsContent = ({
                 Perpigi Organization Website Profile
               </p>
               <p className=""></p>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
