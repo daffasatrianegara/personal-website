69550125073fca2d2fb708afeb2a45c8863f604c
daffasatrianegara <daffasatria036@gmail.com>
2026-01-02 18:26:12 +0700

    format(prettier)

--- src/components/content/current.position.tsx
+++ src/components/content/current.position.tsx
@@ -13,7 +13,9 @@ const CurrentPositionContent = () => {
             <span className="rounded-full bg-emerald w-2 h-2 md:w-3 md:h-3"></span>
           </div>
         </div>
-        <p className="text-sm mt-2 text-justify">{contentData["current-work"]}</p>
+        <p className="text-sm mt-2 text-justify">
+          {contentData["current-work"]}
+        </p>
       </div>
     </Card>
   );
