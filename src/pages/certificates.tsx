539530c436ebb09735e61c4b03148d1fb9c72c28
daffasatrianegara <daffasatria036@gmail.com>
2026-06-22 16:04:38 +0700

    feat(certificates): add filter certificates based on type (completion/competency)

--- src/pages/certificates.tsx
+++ src/pages/certificates.tsx
@@ -7,10 +7,27 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import CertificatesData from "@/contents/certificates/index.json";
+import { useState } from "react";
 
 const CertificatesPage = () => {
+  const [selectedType, setSelectedType] = useState<string | null>(null);
+
   const contentData = CertificatesData.certificates;
+  const certificateTypes = [...new Set(contentData.map((data) => data.type))];
+
+  const filteredData =
+    selectedType === null
+      ? contentData
+      : contentData.filter((data) => data.type === selectedType);
 
   return (
     <MainLayout title="Verified Certifications | Muhammad Daffa Satria Negara">
@@ -26,13 +43,36 @@ const CertificatesPage = () => {
         </BreadcrumbList>
       </Breadcrumb>
       <div className="my-5">
-        <p className="text-xl md:text-2xl font-semibold">
-          Verified Certifications
-        </p>
+        <div className="w-full flex flex-wrap gap-5 items-center">
+          <p className="text-xl md:text-2xl font-semibold">
+            Verified Certifications
+          </p>
+          <div className="ms-auto max-[452px]:m-0">
+            <Select
+              onValueChange={(value) =>
+                setSelectedType(value === "all" ? null : value)
+              }
+            >
+              <SelectTrigger className="w-[180px] border-[1.5px]">
+                <SelectValue placeholder="Filter by Type" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="all">All Types</SelectItem>
+                  {certificateTypes.map((type, index) => (
+                    <SelectItem key={index} value={type}>
+                      {type.replace(/\b\w/g, (char) => char.toUpperCase())}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
         <div className="w-full border-b mt-2 border-gray-200 dark:border-gray-800"></div>
       </div>
       <div className="flex flex-col gap-8 w-full">
-        {contentData.map((data, index) => (
+        {filteredData.map((data, index) => (
           <div key={index}>
             <a
               className="text-base md:text-xl hover:underline font-medium"
